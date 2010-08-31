9835e07ad4780a2e5e00658c62548b7c2e947563
Patrick Tierney <patrick.l.tierney@gmail.com>
2010-08-30 22:33:35 -0700

    Added matrix clearing to draw of repeat project. Added recursive tree project

---
+++ recursive-tree/src/recursive-tree.cpp
@@ -0,0 +1,99 @@
+
+#include <math.h>
+
+#include <cinder/app/AppBasic.h>
+#include <cinder/CinderMath.h>
+#include <cinder/Rand.h>
+
+class RecursiveTree : public ci::app::AppBasic {
+public:
+    void prepareSettings(Settings* settings);
+    void setup();
+    void draw();
+    void seed1(float dotSize, float angle, float x, float y);
+    void seed2(float dotSize, float angle, float x, float y);
+
+private:
+    int width;
+    int height;
+    float dotSize;
+    float angleOffsetA;
+    float angleOffsetB;
+};
+
+void RecursiveTree::prepareSettings(Settings* settings) {
+    width = 900;
+    height = 600;
+    dotSize = 9;
+    settings->setWindowSize(width, height);
+    settings->setFrameRate(1.0f);  // Redraw the tree once a second
+}
+
+void RecursiveTree::setup() {
+    ci::gl::color(ci::Color().black());
+    glEnable(GL_SMOOTH);
+
+    angleOffsetA = ci::toRadians(1.5f);
+    angleOffsetB = ci::toRadians(50.0f);
+}
+
+void RecursiveTree::draw() {
+    ci::gl::setMatricesWindow(getWindowSize());
+    ci::gl::clear(ci::Color().white());              // White background
+    ci::gl::translate(ci::Vec2f(width/2, height));   // Move to the center, bottom of the screen
+    seed1(dotSize, ci::toRadians(270.0f), 0, 0);     // Start the tree
+}
+
+void RecursiveTree::seed1(float dotSize, float angle, float x, float y) {
+
+    if (dotSize > 1.0) {
+
+        // Create a random numbers between 0 and 1
+        float r = ci::Rand::randFloat(0, 1.0f);  
+
+        // 02% chance this will happen
+        if (r > 0.02) {
+            ci::gl::drawSolidCircle(ci::Vec2f(x, y), dotSize/2);
+            float newx = x + cos(angle) * dotSize;
+            float newy = y + sin(angle) * dotSize;
+            seed1(dotSize * 0.99, angle - angleOffsetA, newx, newy);   
+        }
+        // 98% chance this will happen
+        else {  
+            ci::gl::drawSolidCircle(ci::Vec2f(x, y), dotSize/2);
+            float newx = x + cos(angle);
+            float newy = y + sin(angle);
+            seed2(dotSize * 0.99, angle + angleOffsetA, newx, newy);
+            seed1(dotSize * 0.60, angle + angleOffsetB, newx, newy);
+            seed2(dotSize * 0.50, angle - angleOffsetB, newx, newy);
+        } 
+    }
+}
+
+void RecursiveTree::seed2(float dotSize, float angle, float x, float y) {
+
+    if (dotSize > 1.0) {
+
+        // Create a random numbers between 0 and 1
+        float r = ci::Rand::randFloat(0, 1.0);
+
+        // 5% chance this will happen
+        if (r > 0.05) {
+            ci::gl::drawSolidCircle(ci::Vec2f(x, y), dotSize/2);
+            float newx = x + cos(angle) * dotSize;
+            float newy = y + sin(angle) * dotSize;
+            seed2(dotSize * 0.99, angle + angleOffsetA, newx, newy);
+        } 
+        // 95% chance this will happen
+        else {
+            ci::gl::drawSolidCircle(ci::Vec2f(x, y), dotSize/2);
+            float newx = x + cos(angle);
+            float newy = y + sin(angle);
+            seed1(dotSize * 0.99, angle + angleOffsetA, newx, newy);  
+            seed2(dotSize * 0.60, angle + angleOffsetB, newx, newy);
+            seed1(dotSize * 0.50, angle - angleOffsetB, newx, newy);
+        }
+    }
+}
+
+CINDER_APP_BASIC(RecursiveTree, ci::app::RendererGl)
--- repeat/src/repeat.cpp
+++ repeat/src/repeat.cpp
@@ -4,90 +4,91 @@
 
 class Repeat : public ci::app::AppBasic {
 public:
-  void prepareSettings(Settings* settings);
-  void setup();
-  void draw();
+    void prepareSettings(Settings* settings);
+    void setup();
+    void draw();
 
-  void mouseDown(ci::app::MouseEvent);
+    void mouseDown(ci::app::MouseEvent);
 
 private:
-  void arc(const ci::Vec2f& center, float radius, float start, float stop);
-  float map(float val, float inMin, float inMax, float outMin, float outMax);
+    void arc(const ci::Vec2f& center, float radius, float start, float stop);
+    float map(float val, float inMin, float inMax, float outMin, float outMax);
 
-  int width;
-  int height;
-  int option;
+    int width;
+    int height;
+    int option;
 };
 
 void Repeat::prepareSettings(Settings* settings) {
-  width = 400;
-  height = 300;
-  settings->setWindowSize(width, height);
+    width = 400;
+    height = 300;
+    settings->setWindowSize(width, height);
 }
 
 void Repeat::setup() {
-  option = 1;
-  glEnable(GL_LINE_SMOOTH);
-  glLineWidth(1.2f);
-  ci::gl::color(ci::Color().black());
+    option = 1;
+    glEnable(GL_SMOOTH);
+    glLineWidth(1.2f);
+    ci::gl::color(ci::Color().black());
 }
 
 void Repeat::draw() {
-  ci::gl::clear(ci::Color().white());
+    ci::gl::setMatricesWindow(getWindowSize());
+    ci::gl::clear(ci::Color().white());
 
-  if (option == 1) {
-    // Option 1: Stitches
-    for (int x = 50; x <= width-50; x += 20) {
-      for (int y = 50; y <= height-50; y+=20) {
-        ci::gl::drawLine(ci::Vec2f(x-5, y-5), ci::Vec2f(x+5, y+5));
-        ci::gl::drawLine(ci::Vec2f(x+5, y-5), ci::Vec2f(x-5, y+5));
-      }
-    }
-  }
-  else if (option == 2) {
-    // Option 2: Perspective
-    for (int x = 50; x <= width-50; x += 20) {
-      for (int y = 50; y <= height-50; y+=20) {
-        ci::gl::drawLine(ci::Vec2f(x, y), ci::Vec2f(width/2, height/2));
-      }
-    }
-  }
-  else if (option == 3) {
-    // Option 3: Overlapping circles
-    for (int x = 50; x <= width-50; x += 20) {
-      for (int y = 50; y <= height-50; y+=20) {
-        ci::gl::drawStrokedCircle(ci::Vec2f(x, y), 40/2);
-      }
-    }
-  }
-  else if (option == 4) {
-    // Option 4: Rotating arcs
-    int count = 120;
-    for (int x = 50; x <= width-50; x += 20) {
-      for (int y = 50; y <= height-50; y +=20) {
-        float s = map(count, 120, 0, 0, M_PI*2*2);
-        arc(ci::Vec2f(x, y), 14/2, s, s + M_PI);
-        count--;
-      }
-    }
-  }
-  else if (option == 5) {
-    // Option 5: Groups of five
-    for (int x = 50; x < width-50; x += 20) {
-      for (int y = 50; y < height-50; y+=20) {
-        //rect(x-10, y-10, 22, 22);
-        for (int i = 0; i < 16; i+=4) {
-          ci::gl::drawLine(ci::Vec2f(x + i, y), ci::Vec2f(x + i, y + 12));
+    if (option == 1) {
+        // Option 1: Stitches
+        for (int x = 50; x <= width-50; x += 20) {
+            for (int y = 50; y <= height-50; y+=20) {
+                ci::gl::drawLine(ci::Vec2f(x-5, y-5), ci::Vec2f(x+5, y+5));
+                ci::gl::drawLine(ci::Vec2f(x+5, y-5), ci::Vec2f(x-5, y+5));
+            }
         }
-        ci::gl::drawLine(ci::Vec2f(x, y), ci::Vec2f(x + 12, y + 12));
-      }
     }
-  }
+    else if (option == 2) {
+        // Option 2: Perspective
+        for (int x = 50; x <= width-50; x += 20) {
+            for (int y = 50; y <= height-50; y+=20) {
+                ci::gl::drawLine(ci::Vec2f(x, y), ci::Vec2f(width/2, height/2));
+            }
+        }
+    }
+    else if (option == 3) {
+        // Option 3: Overlapping circles
+        for (int x = 50; x <= width-50; x += 20) {
+            for (int y = 50; y <= height-50; y+=20) {
+                ci::gl::drawStrokedCircle(ci::Vec2f(x, y), 40/2);
+            }
+        }
+    }
+    else if (option == 4) {
+        // Option 4: Rotating arcs
+        int count = 120;
+        for (int x = 50; x <= width-50; x += 20) {
+            for (int y = 50; y <= height-50; y +=20) {
+                float s = map(count, 120, 0, 0, M_PI*2*2);
+                arc(ci::Vec2f(x, y), 14/2, s, s + M_PI);
+                count--;
+            }
+        }
+    }
+    else if (option == 5) {
+        // Option 5: Groups of five
+        for (int x = 50; x < width-50; x += 20) {
+            for (int y = 50; y < height-50; y+=20) {
+                //rect(x-10, y-10, 22, 22);
+                for (int i = 0; i < 16; i+=4) {
+                    ci::gl::drawLine(ci::Vec2f(x + i, y), ci::Vec2f(x + i, y + 12));
+                }
+                ci::gl::drawLine(ci::Vec2f(x, y), ci::Vec2f(x + 12, y + 12));
+            }
+        }
+    }
 }
 
 void Repeat::mouseDown(ci::app::MouseEvent /*event*/) {
-  option++;
-  if (option > 5) option = 1;
+    option++;
+    if (option > 5) option = 1;
 }
 
 float Repeat::map(float val, float inMin, float inMax, float outMin, float outMax) {
@@ -104,7 +105,7 @@ void Repeat::arc(const ci::Vec2f& center, float radius, float start, float stop)
     int numVerts = (endSegments-startSegments)*2;
     if (numVerts < 2) numVerts = 2;
     GLfloat* verts = new float[numVerts];
-    
+
     int i = 0;
     for(int s = startSegments; s < endSegments; s++) {
         float t = s / (float)numSegments * 2.0f * 3.14159f;
@@ -112,7 +113,7 @@ void Repeat::arc(const ci::Vec2f& center, float radius, float start, float stop)
         verts[i*2+1] = center.y + ci::math<float>::sin(t) * radius;
         i++;
     }
-    
+
     glEnableClientState(GL_VERTEX_ARRAY);
     glVertexPointer(2, GL_FLOAT, 0, verts);
     glDrawArrays(GL_LINE_STRIP, 0, numVerts / 2);
